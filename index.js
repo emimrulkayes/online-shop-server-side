4305de455dc4bb1714318a89994c22a57f186846
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-05-06 03:34:21 +0600

    added data in mongodb

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bfzit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+
+        await client.connect();
+        const productCollection = client.db('onlineShop').collection('product');
+
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Hello World !');
 });
 
+// Listen app
 app.listen(port,  () => {
     console.log('server is running', port);
 });
