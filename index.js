f50ab106c27cf0766a42e8ab2adc1f9ab26bf12b
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-05-06 02:10:05 +0600

    server setup and print Hello world !

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World !');
+});
+
+app.listen(port,  () => {
+    console.log('server is running', port);
+});
